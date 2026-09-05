c465e9c2f33d2e3d8c3debf9ce116b9e1a2fb592
Javier Acuña <jacuyt2006@gmail.com>
2026-09-05 13:50:43 -0500

    Actividad 1 subida

---
+++ LibroCalificaciones_1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibroCalificaciones_1
+{
+    internal class LibroCalificacion
+    {
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido al libro de calificaciones");
+        }
+    }
+}
---
+++ LibroCalificaciones_1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LibroCalificaciones_1
+{
+    internal class PruebaLibroCalificaciones_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            LibroCalificacion MyLibro = new LibroCalificacion();
+            MyLibro.MostrarMensaje();
+        }
+    }
+}
