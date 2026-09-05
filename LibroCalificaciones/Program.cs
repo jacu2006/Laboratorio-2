991c6b45fdaf8de73c0aa17eb44391238a82f889
Javier Acuña <jacuyt2006@gmail.com>
2026-09-05 13:54:41 -0500

    Actividad 3 subida

---
+++ LibroCalificaciones/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LibroCalificacionesModificadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Crea el objeto LibroCalificaciones
+            //invoca el Constructor de LibroCalificaciones
+
+            LibroCalificaciones myLibro = new LibroCalificaciones("1IL133 Programación en C#");
+            LibroCalificaciones myLibro2 = new LibroCalificaciones("1IL133 HPA III");
+            Console.WriteLine("El nombre del curso es: {0}", myLibro.NombreCurso);
+            Console.WriteLine("El nombre del curso es: {0}", myLibro2.NombreCurso);
+
+            //Pide y lee el nombre del Curso
+            Console.WriteLine("\nEscriba el nombre del curso:");
+            string elNombreCurso = Console.ReadLine();
+            myLibro.NombreCurso = elNombreCurso; //Establece el nombre del curso usando una propiedad
+            Console.WriteLine("el nombre del curso es: {0}", myLibro.NombreCurso);
+        }
+    }
+}
