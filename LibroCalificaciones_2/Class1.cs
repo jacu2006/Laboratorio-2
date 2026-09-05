6a8912b7bd26150284899709e1429d1ebad8a0e7
Javier Acuña <jacuyt2006@gmail.com>
2026-09-05 13:51:52 -0500

    Actividad 2 subida

---
+++ LibroCalificaciones_2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibroCalificaciones_2
+{
+    internal class MiLibroCalificaciones
+    {
+        public void MostrarMensaje(string nombreCurso)
+        {
+            Console.WriteLine("¡Bienvenido al libro de calificaciones para: \n{0} !", nombreCurso);
+        }
+    }
+}
---
+++ LibroCalificaciones_2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LibroCalificaciones_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            MiLibroCalificaciones MyLibro = new MiLibroCalificaciones();
+            //Pide el nombre del Curso y lo recibe como entrada
+            Console.WriteLine("Por favor ingrese el nombre del curso: ");
+
+            string nombreDelCurso = Console.ReadLine(); //Lee una línea de Texto
+            Console.WriteLine(); //Imprime en pantalla una línea en blanco
+            //Llama al método de MostrarMensaje de MiLibroCalificaciones
+            //y pasa el nombre del curso como argumento
+
+
+            MyLibro.MostrarMensaje(nombreDelCurso);
+
+        }
+    }
+}
